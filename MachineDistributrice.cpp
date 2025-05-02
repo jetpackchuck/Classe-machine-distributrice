52d109466676cebf844fd059ece885e332664205
jetpackchuck <charlesetremblay@icloud.com>
2025-05-02 11:11:24 -0400

    Update MachineDistributrice.cpp

--- MachineDistributrice.cpp
+++ MachineDistributrice.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 void Machine_distributrice::newMachine() {
 	vector<vector<Case>> cases(string, Produit);
-	list<float> logVentes;
+	list<Produit> logVentes;
 	map<Produit, float> prixProduits;
 }
