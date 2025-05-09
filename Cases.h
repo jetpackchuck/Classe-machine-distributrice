f1b2752a55ce41989307b817934b075cf30f1df9
lilou-1 <lilbuisson@gmail.com>
2025-05-09 18:34:49 +0500

    Add files via upload

--- Cases.h
+++ Cases.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stack>
 #include "produit.h"
+#include "MachineDistributrice.h"
 
 class Cases {
 private:
@@ -11,6 +12,6 @@ private:
 public:
 	Cases(int id, Produit produit, int capacite = 10);
 	void refill(Produit produit, int quantite);
-	void vente();
+	void vente(machineDistributrice machine);
 	bool estVide();
 };
